e9fb4ca8b438fbbb376a387e6aec080c753e7cb0
HugooVer <hugo.vercelloneg@gmail.com>
2023-05-26 21:33:06 +0200

    Map name check ok

--- srcs/map_check.c
+++ srcs/map_check.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_check.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hugo <hugo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:53:22 by hvercell          #+#    #+#             */
-/*   Updated: 2023/05/26 19:42:20 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/05/26 21:32:44 by hugo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,7 @@ int	is_ber_extention(t_arg *a)
 {
 	if (ft_strlen(a->v[1]) <= 4)
 		return (EXIT_FAILURE);
-	printf(" == %lu == \n", *a->v[1] + ft_strlen(a->v[1]) - 3);
-	printf(" == %s == \n", a->v[1]);
-	printf(" == %s == \n", ft_strnstr(a->v[1] + (ft_strlen(a->v[1] - 3)), ".ber", 4));
-	if (ft_strnstr(a->v[1] + (ft_strlen(a->v[1] - 3)), ".ber", 4) == NULL)
+	if ((ft_strnstr(a->v[1] + ft_strlen(a->v[1]) - 4, ".ber", 4)) == NULL)
 		return (EXIT_FAILURE);
-	printf("Map ok\n");
 	return(EXIT_SUCCESS);
 }
