011e21e13a0bc8c91a889320e21cd344b23a5ad9
HugooVer <hugo.vercelloneg@gmail.com>
2023-05-28 19:10:28 +0200

    map check function in the proper file

--- srcs/data_set.c
+++ srcs/data_set.c
@@ -6,7 +6,7 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:47:46 by hvercell          #+#    #+#             */
-/*   Updated: 2023/05/28 19:06:46 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/05/28 19:10:02 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,65 +20,4 @@ void	data_init(int argc, char **argv, t_arg *a, t_data *d)
 	d->map = NULL;
 	d->height = 0;
 	d->width = 0;
-}
-
-void	read_buf(t_data *d, ssize_t *size, char *buf)
-{
-	*size = read(d->map_fd, buf, BUFFER_SIZE);
-	if (*size == -1)
-		error_free(d);
-	if (*size != 0)
-		buf[*size] = '\0';
-}
-
-void	map_count_lines(t_data *d)
-{
-	ssize_t	size;
-	int		i;
-	char	buf[BUFFER_SIZE + 1];
-
-	d->map_fd = open(d->map_name, O_RDONLY);
-	read_buf(d, &size, buf);
-	i = 0;
-	while (size != 0)
-	{
-		while (buf[i] != '\0')
-		{
-			if (buf[i] == '\n')
-				++d->height;
-			++i;
-		}
-		if (buf[i - 1] != '\n')
-			++d->height;
-		read_buf(d, &size, buf);
-		i = 0;
-	}
-	close(d->map_fd);
-}
-
-void	map_conversion(t_data *d)
-{
-	int		idx;
-	char	*line;
-
-	d->map_fd = open(d->map_name, O_RDONLY);
-	d->map = ft_calloc(d->height + 1, sizeof(char *));
-	if (d->map == NULL)
-		error_free(d);
-	line = get_next_line(d->map_fd);
-	idx = 0;
-	while (line != NULL)
-	{
-		d->map[idx] = ft_strdup(line);
-		free(line);
-		line = get_next_line(d->map_fd);
-		++idx;
-	}
-	free(line);
-}
-
-void	map_check(t_data *d)
-{
-	map_count_lines(d);
-	map_conversion(d);
 }
--- srcs/map_check.c
+++ srcs/map_check.c
@@ -6,7 +6,7 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:53:22 by hvercell          #+#    #+#             */
-/*   Updated: 2023/05/28 19:01:44 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/05/28 19:09:55 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,65 @@ int	is_ber_extention(t_arg *a, t_data *d)
 		error_no_free();
 	d->map_name = ft_strnjoin("map/", a->v[1], ft_strlen(a->v[1]));
 	return (EXIT_SUCCESS);
+}
+
+void	read_buf(t_data *d, ssize_t *size, char *buf)
+{
+	*size = read(d->map_fd, buf, BUFFER_SIZE);
+	if (*size == -1)
+		error_free(d);
+	if (*size != 0)
+		buf[*size] = '\0';
+}
+
+void	map_count_lines(t_data *d)
+{
+	ssize_t	size;
+	int		i;
+	char	buf[BUFFER_SIZE + 1];
+
+	d->map_fd = open(d->map_name, O_RDONLY);
+	read_buf(d, &size, buf);
+	i = 0;
+	while (size != 0)
+	{
+		while (buf[i] != '\0')
+		{
+			if (buf[i] == '\n')
+				++d->height;
+			++i;
+		}
+		if (buf[i - 1] != '\n')
+			++d->height;
+		read_buf(d, &size, buf);
+		i = 0;
+	}
+	close(d->map_fd);
+}
+
+void	map_conversion(t_data *d)
+{
+	int		idx;
+	char	*line;
+
+	d->map_fd = open(d->map_name, O_RDONLY);
+	d->map = ft_calloc(d->height + 1, sizeof(char *));
+	if (d->map == NULL)
+		error_free(d);
+	line = get_next_line(d->map_fd);
+	idx = 0;
+	while (line != NULL)
+	{
+		d->map[idx] = ft_strdup(line);
+		free(line);
+		line = get_next_line(d->map_fd);
+		++idx;
+	}
+	free(line);
+}
+
+void	map_check(t_data *d)
+{
+	map_count_lines(d);
+	map_conversion(d);
 }
