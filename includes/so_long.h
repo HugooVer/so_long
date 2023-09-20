3033ab28a33ecd076d5966c7bd44729c630d9d2a
HugooVer <hugo.vercelloneg@gmail.com>
2023-09-20 18:53:55 +0200

    no player working -- exit is solid in th back traking -- a colectible can be locked in some colectibles

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:23:01 by hvercell          #+#    #+#             */
-/*   Updated: 2023/09/19 19:18:51 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/09/20 17:23:58 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ struct s_data
 	int			width;
 	int			height;
 	int			colectible_nb;
+	int			colectible_back;
 	int			colectible_count;
 	int			player_pos[2];
 	int			map_fd;
@@ -89,7 +90,7 @@ void	map_count_lines(t_data *d);
 void	map_conversion(t_data *d);
 void	map_check(t_arg *a, t_data *d);
 
-int		backtrack(int x, int y, char **tab, char looking);
+int		backtrack(int x, int y, char **tab, t_data *d);
 void	look_for_player(t_data *d);
 
 void	error_free(t_data *d);
